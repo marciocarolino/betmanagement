51bd0e35b25a48692ea6b0587d2e2aeb9b9dfde8
DESKTOP-TQVMCDE\marcio <marciocarolino@gmail.com>
2022-04-19 06:13:57 -0300

    continuando tela login

--- betmanagement/src/pages/login/index.jsx
+++ betmanagement/src/pages/login/index.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import "./style.css";
 
 const LoginPage = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("submit");
+  };
+
   return (
     <div id="login">
       <h1 className="title">Login do Sistema</h1>
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <div className="field">
           <label htmlFor="email">Email</label>
           <input type="email" name="email" id="email" />
