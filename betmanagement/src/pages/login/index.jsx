4777f350bfb6486d28a4b5194d22557768bb7603
DESKTOP-TQVMCDE\marcio <marciocarolino@gmail.com>
2022-04-24 22:49:01 -0300

    terminando a tela de login

--- betmanagement/src/pages/login/index.jsx
+++ betmanagement/src/pages/login/index.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("submit");
+    console.log("submit", { email, password });
   };
 
   return (
@@ -13,11 +16,23 @@ const LoginPage = () => {
       <form className="form" onSubmit={handleSubmit}>
         <div className="field">
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
         </div>
         <div className="field">
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="password" />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
         </div>
         <button type="submit">Entrar</button>
       </form>
